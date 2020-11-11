9ef93d260edab222dba9fb633be4c758553269e7
alejandro <aalvarez@justierradelfuego.gov.ar>
2020-11-11 20:58:31 -0300

    cambio puerto

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/', (req, res) => {
+app.get('/marte', (req, res) => {
     res.send('Hello Marte!')
   })
 
